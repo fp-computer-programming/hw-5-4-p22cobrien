a138d22bc55794609500465f84dad6e0eb9d968d
p22cobrien <p22cobrien@fairfieldprep.org>
2021-11-02 09:56:32 -0400

    submit hw5-4.py

---
+++ hw5-4.py
@@ -0,0 +1,31 @@
+# Author: CMOB 11/2/2021
+
+# Question 1
+print("python"[2])
+
+# Question 2
+print("Strings are simply a sequence of characters."[30])
+
+# Question 3
+print(len('excellent'))
+
+# Question 4
+print('apple' in 'Pineapple')
+
+# Question 5
+# if "Peach" > "pear"
+
+# Question 6
+print("x" * 100)
+
+# Question 7
+str(54321)
+
+# Question 8
+print("p" in "Pineapple")
+
+# Question 9
+print("\"Livin\' On a Prayer\" is my friend\'s favorite song.")
+
+# Question 10
+print(len("\"Livin\' On a Prayer\" is my friend\'s favorite song."))
